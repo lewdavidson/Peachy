6fed21346de3e0ee744ae2816ed3f462351058aa
Lew Davidson <lewdavidson47@gmail.com>
2018-02-23 10:49:19 -0800

    add initial development files and instantiate developer environment

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <HashRouter>
+        <Component/>
+      </HashRouter>
+    </AppContainer>,
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
+
+/*eslint-disable */
+if (module.hot) {
+  module.hot.accept('./Components/App', () => {
+    render(App);
+  });
+}
+/*eslint-enable */
